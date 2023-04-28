012a88ced7c75dd8453ae52025b7a68c27ef680e
enrik-rucaj <rucajenrik111@gmail.com>
2023-04-28 17:10:52 +0200

    BlockUiTest

---
+++ src/components/__test__/BlockUIObserver.test.tsx
@@ -0,0 +1,26 @@
+import { jest } from '@jest/globals';
+import { render } from "@testing-library/react";
+import BlockUIObserver from "../BlockUIObserver";
+
+describe('BlockUIObserver', () => {
+    it('renders the progress spinner when shouldBlock is true', () => {
+        const { getByRole } = render(
+            <BlockUIObserver shouldBlock={() => true}>
+                <div>Child Component</div>
+            </BlockUIObserver>
+        );
+
+        //the progress spinner has role alert
+        expect(getByRole('alert')).toBeInTheDocument();
+    });
+
+    it('renders the children when shouldBlock is false', () => {
+        const { getByText } = render(
+            <BlockUIObserver shouldBlock={() => false}>
+              <div>Child Component</div>
+            </BlockUIObserver>
+          );
+      
+        expect(getByText('Child Component')).toBeInTheDocument();
+    });
+});
