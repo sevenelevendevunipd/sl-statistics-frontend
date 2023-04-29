e56968a871871515b6f9ea4d5bbe2f11ba4b8150
MrBrune01 <brunello.mattia2001@gmail.com>
2023-04-29 15:16:23 +0200

    Prettier che fa cose

--- src/components/__test__/BlockUIObserver.test.tsx
+++ src/components/__test__/BlockUIObserver.test.tsx
@@ -1,26 +1,25 @@
-import { jest } from '@jest/globals';
 import { render } from "@testing-library/react";
 import BlockUIObserver from "../BlockUIObserver";
 
-describe('BlockUIObserver', () => {
-    it('renders the progress spinner when shouldBlock is true', () => {
-        const { getByRole } = render(
-            <BlockUIObserver shouldBlock={() => true}>
-                <div>Child Component</div>
-            </BlockUIObserver>
-        );
+describe("BlockUIObserver", () => {
+  it("renders the progress spinner when shouldBlock is true", () => {
+    const { getByRole } = render(
+      <BlockUIObserver shouldBlock={() => true}>
+        <div>Child Component</div>
+      </BlockUIObserver>
+    );
 
-        //the progress spinner has role alert
-        expect(getByRole('alert')).toBeInTheDocument();
-    });
+    //the progress spinner has role alert
+    expect(getByRole("alert")).toBeInTheDocument();
+  });
 
-    it('renders the children when shouldBlock is false', () => {
-        const { getByText } = render(
-            <BlockUIObserver shouldBlock={() => false}>
-              <div>Child Component</div>
-            </BlockUIObserver>
-          );
-      
-        expect(getByText('Child Component')).toBeInTheDocument();
-    });
+  it("renders the children when shouldBlock is false", () => {
+    const { getByText } = render(
+      <BlockUIObserver shouldBlock={() => false}>
+        <div>Child Component</div>
+      </BlockUIObserver>
+    );
+
+    expect(getByText("Child Component")).toBeInTheDocument();
+  });
 });
--- src/components/__test__/StackedBarChart.test.tsx
+++ src/components/__test__/StackedBarChart.test.tsx
@@ -1,32 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import StackedBarChart from "../StackedBarChart";
 import { useThemeName } from "../ThemeSwitcher";
-import MainView from "../../views/MainView";
 
 jest.mock("../ThemeSwitcher", () => ({
-    useThemeName: jest.fn(),
+  useThemeName: jest.fn(),
 }));
 
 describe("StackedBarChart component", () => {
-    const dataset = [
-        { month: "January", apples: "10", oranges: "5", total: "15" },
-        { month: "February", apples: "15", oranges: "10", total: "25" },
-        { month: "March", apples: "20", oranges: "15", total: "35" },
-    ];
+  const dataset = [
+    { month: "January", apples: "10", oranges: "5", total: "15" },
+    { month: "February", apples: "15", oranges: "10", total: "25" },
+    { month: "March", apples: "20", oranges: "15", total: "35" },
+  ];
 
-    beforeEach(() => {
-        (useThemeName as jest.Mock).mockReturnValue("dark");
-    });
+  beforeEach(() => {
+    (useThemeName as jest.Mock).mockReturnValue("dark");
+  });
 
-    it("should render a message if the dataset is empty", () => {
-        const { getByText } = render(
-            <StackedBarChart dataset={[]} xKey="month" />
-        );
-        expect(getByText("No entries to plot!")).toBeInTheDocument();
-    });
+  it("should render a message if the dataset is empty", () => {
+    const { getByText } = render(<StackedBarChart dataset={[]} xKey="month" />);
+    expect(getByText("No entries to plot!")).toBeInTheDocument();
+  });
 
-    it('should render a bar chart with the correct data', () => {
-        const { container } = render(<StackedBarChart dataset={dataset} xKey="month" />);
-        expect(container.querySelector('.echarts-for-react')).toBeInTheDocument();
-    });
+  it("should render a bar chart with the correct data", () => {
+    const { container } = render(
+      <StackedBarChart dataset={dataset} xKey="month" />
+    );
+    expect(container.querySelector(".echarts-for-react")).toBeInTheDocument();
+  });
 });
