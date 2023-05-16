6be926fc99bef3d6ca816b7e909e9a38632693cb
Augusto Zanellato <augusto.zanellato@gmail.com>
2023-05-17 00:00:23 +0200

    firmware chart viewmodel

---
+++ src/viewmodels/tabs/FirmwareChartViewModel.ts
@@ -0,0 +1,27 @@
+import { ListBoxChangeEvent } from "primereact/listbox";
+import { useRootStore } from "../../stores/RootStore";
+import { runInAction } from "mobx";
+
+export type IFirmwareChartViewModel = ReturnType<typeof FirmwareChartViewModel>;
+
+export const FirmwareChartViewModel = () => {
+  const { firmwareChartDataStore, chartFilterStore } = useRootStore();
+  return {
+    chartData: () => firmwareChartDataStore.data,
+    selectableCodes: () => chartFilterStore.selectableCodes,
+    selectedCodes: () => firmwareChartDataStore.selectedCodes,
+    disableCodeOption: (option: string) =>
+      firmwareChartDataStore.selectedCodes.length >= 7 &&
+      !firmwareChartDataStore.selectedCodes.includes(option),
+    onCodeSelectionChange: (e: ListBoxChangeEvent) =>
+      runInAction(() => (firmwareChartDataStore.selectedCodes = e.value)),
+
+    selectableFirmwares: () => chartFilterStore.selectableFirmwares,
+    selectedFirmwares: () => firmwareChartDataStore.selectedFirmwares,
+    onFirmwareSelectionChange: (e: ListBoxChangeEvent) =>
+      runInAction(() => (firmwareChartDataStore.selectedFirmwares = e.value)),
+    
+    hasError: () => chartFilterStore.hasError || firmwareChartDataStore.hasError,
+    error: () => chartFilterStore.error ?? firmwareChartDataStore.error,
+  };
+};
